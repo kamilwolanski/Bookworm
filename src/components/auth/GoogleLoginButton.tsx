400d32695903ed959cb0ac3a7ccca1e64268dcb1
Kamil <kwolanski3@gmail.com>
2025-10-16 12:02:05 +0200

    restyle google log in button

--- src/components/auth/GoogleLoginButton.tsx
+++ src/components/auth/GoogleLoginButton.tsx
@@ -3,6 +3,7 @@
 import { signIn } from 'next-auth/react';
 import googleLogo from '@/app/assets/google_logo.svg.png';
 import Image from 'next/image';
+import { Button } from '../ui/button';
 
 export default function GoogleLoginButton({
   callbackUrl,
@@ -10,12 +11,13 @@ export default function GoogleLoginButton({
   callbackUrl?: string;
 }) {
   return (
-    <button
+    <Button
+      variant="ghost"
       onClick={() => signIn('google', { callbackUrl: callbackUrl ?? '/books' })}
-      className=" border border-gray-300 h-12 rounded-2xl px-6 py-2 w-full flex items-center cursor-pointer gap-3 text-sm font-medium shadow-sm hover:bg-gray-100 transition"
+      className="border h-12 rounded-2xl px-6 py-2 w-full flex items-center cursor-pointer gap-3 text-sm font-medium shadow-sm transition"
     >
       <Image src={googleLogo} width={16} height={16} alt="google" />
       Kontynuuj z Google
-    </button>
+    </Button>
   );
 }
