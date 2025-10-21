0896230ac502ce06c0ad899ff83982d49ad4ccce
Kamil <kwolanski3@gmail.com>
2025-10-21 19:59:44 +0200

    block for gptbot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,6 +4,10 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
+        userAgent: 'GPTBot',
+        disallow: '/',
+      },
+      {
         userAgent: '*',
         allow: '/',
         disallow: ['/admin', '/admin/*'],
