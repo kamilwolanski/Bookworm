cea6ecf17acc69c50f993413f2cd81d16ffc8fc8
Kamil <kwolanski3@gmail.com>
2026-01-14 11:21:56 +0100

    restored analytics

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/next';
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import SWRConfigProvider from "../services/SWRConfigProvider";
@@ -72,6 +73,7 @@ export default function RootLayout({
             </SWRConfigProvider>
           </SessionProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
